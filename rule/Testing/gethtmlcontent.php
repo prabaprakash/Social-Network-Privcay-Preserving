671580c90fac60e24a37bde1cf8769c41ad31486
praba <prabaprakash@outlook.com>
2016-11-26 02:37:18 +0530

    Dev

--- rule/Testing/gethtmlcontent.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$file = file_get_contents('https://in.linkedin.com/in/prabaprakash');
-echo strip_tags($file);
-
-?>
--- rule/db.php
+++
@@ -1,2 +0,0 @@
-<?php
-$pdo=new PDO('mysql:dbname=abi;host=127.0.0.1:3306','root','');
